b3363061909c959cb4b76b424f444f6c04e1997f
yaohsuanlin <87404630+yaohsuanlin@users.noreply.github.com>
2021-07-14 11:32:38 +0800

    update code
    try update code and check git system

--- sketch_jul14a.ino
+++ sketch_jul14a.ino
@@ -9,7 +9,7 @@ int ledState = LOW;             // ledState used to set the LED
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // constants won't change:
-const long interval_on = 2500;           // interval at which to blink (milliseconds)
+const long interval_on = 2000;           // interval at which to blink (milliseconds)
 const long interval_off= 1000;           // interval at which to blink (milliseconds)
 
 void setup() {
